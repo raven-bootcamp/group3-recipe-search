9831dadc093eb5d036c8737d7735a67582504b35
Jim Sharples <jamie.sharples@gmail.com>
2021-07-05 20:19:33 +1000

    Added task "Search returns a recipe in standard format"

--- public/js/App.js
+++ public/js/App.js
@@ -3,7 +3,27 @@ import Controller from "./Controller.js";
 
 class App {
     constructor() {
-        this.controller = new Controller(document,window.localStorage);
+        this.controller = new Controller(this,window.localStorage);
+    }
+
+    /*  This method is called from the Controller when recipe search results have returned */
+    receiveRecipeSearchResults(recipes) {
+        /*
+           recipes is an array of objects that contain the following information:
+           id  - EDAMAM id - will be needed for a new call
+           name,
+           imageURL,
+           calories,
+           ingredients - an array of strings
+           mealType - string [Breakfast|Dinner|etc]
+           diet - string [Balanced|Gluten-free|etc]
+
+           if the array is empty (length 0) then there are no matching recipes or there was a web error (can't get data)
+        */
+        // TO-DO display the recipes on the user interface
+        logger.log("Received recipes for display from a search",1);
+        logger.log(recipes,100);
+
     }
 }
 /* turn on console messages for development*/
@@ -11,6 +31,6 @@ logger.setOn();
 logger.setLevel(100);
 
 let app = new App();
-/* test API call */
-app.controller.__testAPICall();
+app.controller.searchForRecipes();
+
 
