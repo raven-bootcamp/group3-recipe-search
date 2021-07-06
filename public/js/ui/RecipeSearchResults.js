bc1313d52739f8c26fd18aa17287f9b026cd3409
Nick Lee <raven1300@hotmail.com>
2021-07-06 21:34:27 +1000

    adding population for search results

--- public/js/ui/RecipeSearchResults.js
+++ public/js/ui/RecipeSearchResults.js
@@ -10,5 +10,27 @@ export default class RecipeSearchResults {
         logger.log("Rendering search results",10);
         logger.log(arrayOfRecipes,10);
 
+        var displaySearchResults = function (results) {
+            if (results.length === 0) {
+                resultsList.textContent = "No recipes found!";
+                return;
+            }
+
+            for (var i = 0; i < results; i++) {
+                var recipeName = results[i].recipeName // UPDATE KEY
+
+                var recipeCard = document.createElement("div");
+                recipeCard.classList = "card";
+
+                var recipeCardTitle = document.createElement("p");
+                recipeCardTitle.classList = "card-header-title is-size-4";
+                
+                var 
+
+            }
+        
+            
+        }
+
     }
 }
