de9213b16b96aafd550c0055096b74d126121640
Jim Sharples <jamie.sharples@gmail.com>
2021-07-07 06:43:21 +1000

    Working on using JSX for components

--- public/js/ui/ShoppingList.js
+++ public/js/ui/ShoppingList.js
@@ -1,41 +1,46 @@
 import logger from "../util/SimpleDebug.js";
 import DOMUtil from "../util/ui/DOMUtil.js";
 
-export default class ShoppingList {
-    constructor(application,document) {
-        this.document = document;
-        this.application = application;
-        this.domutil = new DOMUtil(document);
-        this.hide = this.hide.bind(this);
+var ShoppingList = /*#__PURE__*/function () {
+  function ShoppingList(application, document) {
+    this.document = document;
+    this.application = application;
+    this.domutil = new DOMUtil(document);
+    this.hide = this.hide.bind(this);
+    var closeButtonEl = this.document.getElementById("close-shopping-list-delete-button");
+    closeButtonEl.addEventListener("click", this.hide);
+    closeButtonEl = this.document.getElementById("close-shopping-list-button");
+    closeButtonEl.addEventListener("click", this.hide);
+  }
 
-        let closeButtonEl = this.document.getElementById("close-shopping-list-delete-button");
-        closeButtonEl.addEventListener("click",this.hide);
-        closeButtonEl = this.document.getElementById("close-shopping-list-button");
-        closeButtonEl.addEventListener("click",this.hide);
-    }
+  var _proto = ShoppingList.prototype;
 
-    render(shoppingList) {
-        logger.log("Rendering shopping list",10);
-        logger.log(shoppingList,10);
-        // clear the current shopping list and redraw dynamically
-        let element = this.document.getElementById("shopping-list-content");
-        this.domutil.removeAllChildNodes(element);
-        // add the ingredients as buttons under list items
-        for (let index = 0;index < shoppingList.length;index++) {
-            let buttonEl = this.domutil.addDeleteButtonAsListItemOfParent(shoppingList[index],element);
-            buttonEl.addEventListener("click",this.application.handleEventRemoveIngredientFromShoppingList);
-        }
-    }
+  _proto.render = function render(shoppingList) {
+    logger.log("Rendering shopping list", 10);
+    logger.log(shoppingList, 10); // clear the current shopping list and redraw dynamically
 
-    show() {
-        logger.log("Showing shopping list",10);
-        let element = this.document.getElementById("shopping-list");
-        element.classList.add("is-active");
-    }
+    var element = this.document.getElementById("shopping-list-content");
+    this.domutil.removeAllChildNodes(element); // add the ingredients as buttons under list items
 
-    hide(event) {
-        logger.log("Hiding shopping list",10);
-        let element = this.document.getElementById("shopping-list");
-        element.classList.remove("is-active");
+    for (var index = 0; index < shoppingList.length; index++) {
+      var buttonEl = this.domutil.addDeleteButtonAsListItemOfParent(shoppingList[index], element);
+      buttonEl.addEventListener("click", this.application.handleEventRemoveIngredientFromShoppingList);
     }
-}
+  };
+
+  _proto.show = function show() {
+    logger.log("Showing shopping list", 10);
+    var element = this.document.getElementById("shopping-list");
+    element.classList.add("is-active");
+  };
+
+  _proto.hide = function hide(event) {
+    logger.log("Hiding shopping list", 10);
+    var element = this.document.getElementById("shopping-list");
+    element.classList.remove("is-active");
+  };
+
+  return ShoppingList;
+}();
+
+export { ShoppingList as default };
